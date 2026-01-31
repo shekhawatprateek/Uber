92af5afd9ad3fa0f191014a923432834bdb0eee7
shekhawatprateek <princeshekhawat78@gmail.com>
2026-01-31 20:34:36 +0530

    first commit

---
+++ Backend/server.js
@@ -0,0 +1,14 @@
+const http = require('http')
+const app = require('./app')
+
+const port = process.env.PORT || 3000;
+
+
+ 
+
+const server = http.createServer(app)
+
+server.listen(port, (req, res) => {
+    console.log(`Server is running on ${port}`);
+    
+})
